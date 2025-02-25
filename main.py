df26d1289634fcd12c807aeca72158a07b3f058a
Tanya Maximova <tanya.maksimova.2004@inbox.ru>
2025-02-25 21:00:27 +0300

    Добавлен новый файл

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+form1 = Tk()
+W, H, L, T = 320, 240, 600, 250
+form1.geometry(f"{W}x{H}+{L}+{T}")
+form1.config(bg="cyan")
+
+frame1 = Frame()
+frame1.pack(side=LEFT)
+frame2 = Frame()
+frame2.pack()
+
+label1 = Label(frame1, text="label1")
+label1.pack(side="left")
+label2 = Label(frame1, text="label2")
+label2.pack()
+label3 = Label(frame2, text="label3")
+label3.pack()
+
+form1.mainloop()
